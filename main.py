21cbbb8a45932135429ac13d1c525cf8f9783fb4
Taulanttt <101142739+Taulanttt@users.noreply.github.com>
2022-12-22 21:46:36 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import streamlit as st
+
+st.title("Calculator")
+
+num1 = st.number_input("Enter the first number:")
+num2 = st.number_input("Enter the second number:")
+
+operation = st.selectbox("Select an operation:", ["Add", "Subtract", "Multiply", "Divide"])
+
+if operation == "Add":
+    result = num1 + num2
+elif operation == "Subtract":
+    result = num1 - num2
+elif operation == "Multiply":
+    result = num1 * num2
+else:
+    result = num1 / num2
+if st.button('Result'): 
+    st.write(f"Result: {result}")
+
+
